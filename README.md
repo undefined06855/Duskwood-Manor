3b49daf0adafa81c6185cc94fc949c80c65acb76
undefined06855 <currentcashew57@gmail.com>
2026-06-21 22:03:36 +0100

    make the readme Swag

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Duskwood Manor
-
 A mod for the [Wathe](https://modrinth.com/mod/wathe) map Duskwood Manor by somewhat_grand!
 
-## Setup
+<img src="manor.png" width=320>
 
+## Setup
 For setup instructions, please see the [Fabric Documentation page](https://docs.fabricmc.net/develop/getting-started/creating-a-project#setting-up) related to the IDE that you are using.
--- src/main/java/dev/undefined0/duskwoodmanor/events/GameEventsListener.java
+++ src/main/java/dev/undefined0/duskwoodmanor/events/GameEventsListener.java
@@ -3,6 +3,14 @@ package dev.undefined0.duskwoodmanor.events;
 import dev.doctor4t.wathe.api.WatheGameModes;
 import dev.doctor4t.wathe.api.event.GameEvents;
 import dev.doctor4t.wathe.cca.GameWorldComponent;
+import dev.doctor4t.wathe.cca.MapVariablesWorldComponent;
+import dev.doctor4t.wathe.cca.WorldBlackoutComponent.BlackoutDetails;
+import dev.doctor4t.wathe.game.GameConstants;
+import dev.doctor4t.wathe.index.WatheProperties;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
 // doctor4t this is a great mod and i love it but can the time of day system be a LITTLE better??
@@ -22,5 +30,20 @@ public class GameEventsListener implements GameEvents.OnFinishFinalize, GameEven
     public void onFinishFinalize(World world, GameWorldComponent gameComponent) {
         if (world.isClient) return;
         world.getServer().getWorld(world.getRegistryKey()).setTimeOfDay(12800);
+
+        // reset all the shit
+        MapVariablesWorldComponent areas = MapVariablesWorldComponent.KEY.get(world);
+
+        Box area = areas.playArea;
+        for (int x = (int) area.minX; x <= (int) area.maxX; x++) {
+            for (int y = (int) area.minY; y <= (int) area.maxY; y++) {
+                for (int z = (int) area.minZ; z <= (int) area.maxZ; z++) {
+                    BlockPos pos = new BlockPos(x, y, z);
+                    BlockState state = world.getBlockState(pos);
+
+                    
+                }
+            }
+        }
     }
 }
