5bca8cd75c209b619021d47df7fa2ff26b0b9186
undefined06855 <currentcashew57@gmail.com>
2026-07-03 16:35:06 +0100

    bump cca version too might as well

--- src/main/java/dev/undefined0/duskwoodmanor/cca/CCAModEnforcer.java
+++ src/main/java/dev/undefined0/duskwoodmanor/cca/CCAModEnforcer.java
@@ -12,7 +12,7 @@ import net.minecraft.world.World;
 // easy way to force the mod on client and server
 
 public class CCAModEnforcer implements AutoSyncedComponent {
-    public static final ComponentKey<CCAModEnforcer> KEY = ComponentRegistry.getOrCreate(DuskwoodManor.id("really_important_i_promise_v4"), CCAModEnforcer.class);
+    public static final ComponentKey<CCAModEnforcer> KEY = ComponentRegistry.getOrCreate(DuskwoodManor.id("really_important_i_promise_v5"), CCAModEnforcer.class);
 
     public CCAModEnforcer(World world) {
     }
